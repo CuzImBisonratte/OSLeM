c70aae3035c5c70beea73fbfed3949cd9dc5e0d6
Konstantin Protzen <code@kondev.de>
2025-09-13 01:36:19 +0200

    🎉  Start project

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<?php include './res/snippets/head.html'; ?>
+</head>
+
+<body>
+	<header>
+		<div class="header-left">
+			<img src="/res/images/logo.light.svg" class="light-hidden logo" alt="">
+			<img src="/res/images/logo.svg" class="dark-hidden logo" alt="">
+			<h1>OSLeM</h1>
+		</div>
+		<div class="header-right">
+			<img src="https://placehold.co/100/FFF/000" class="avatar" alt="User profile picture">
+		</div>
+	</header>
+	<nav></nav>
+	<main></main>
+</body>
+
+</html>
